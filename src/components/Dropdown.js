3e71bbc02f8b207df5ddd7919f9df47822c18875
navaneethmadhav <madnav114@gmail.com>
2023-05-02 20:25:57 +0530

    Fetching Data to Home

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import React,{useState, useEffect} from 'react'
 import './Dropdown.css'
+import { Table } from 'react-bootstrap'
 
 function Dropdown() {
 
     const [tags, setTags] = useState([])
+    const [singleTag, setSingleTag] = useState([])
 
     useEffect(function(){
         axios.get('https://api.quotable.io/tags')
@@ -12,16 +14,40 @@ function Dropdown() {
         .then(error => console.log(error))
     },[])
 
+    const onddlChange = (e) =>{
+        // alert(e.target.value)
+        axios.get('https://api.quotable.io/random')
+        .then(response => setSingleTag(response.data))
+        .then(error => console.log(error))
+    }
+
   return (
-    <div className='select-container'>
+    <div className='select-container' onChange={onddlChange}>
         <select className='select-box'>
             <option value='0'>-- Select Tag --</option>
             {
                 tags.map(tag =>
-                    <option key={tag.id} value={tag.id}>{tag.name}</option>
+                    <option key={tag._id} value={tag._id}>{tag.name}</option>
                 )
             }
         </select>
+
+        {/* <Table striped bordered>
+            <thead>
+                <tr>
+                    <td>id</td>
+                    <td>name</td>
+                    <td>Author</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{singleTag._id}</td>
+                    <td>{singleTag.content}</td>
+                    <td>{singleTag.author}</td>
+                </tr>
+            </tbody>
+        </Table> */}
     </div>
   )
 }
